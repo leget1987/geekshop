7a7040a3a38cfb973aa1ea56a8bc4987f24a9530
leget1987 <lobanov.leget@yandex.ru>
2020-10-08 19:17:46 +0300

    Lesson 3. ORM start

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
-
+from django.conf import settings
+from django.conf.urls.static import static
 import mainapp.views as mainapp
 
 urlpatterns = [
@@ -14,3 +15,6 @@ urlpatterns = [
     path("products/classic", mainapp.products, name="products_classic"),
     path("contact/", mainapp.contact, name="contact"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
