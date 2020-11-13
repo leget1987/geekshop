258ec6baa24cc171675f4713a580259ad508a5c3
leget1987 <60894469+leget1987@users.noreply.github.com>
2020-11-13 19:52:27 +0300

    Lesson 2.4 (#12)
    Зыкрытие pull request

--- basketapp/models.py
+++ basketapp/models.py
@@ -2,6 +2,15 @@ from django.conf import settings
 from django.db import models
 
 from mainapp.models import Product
+from django.shortcuts import get_object_or_404
+
+
+# class BasketQuerySet(models.QuerySet):
+#     def delete(self, *args, **kwargs):
+#         for object in self:
+#             object.product.quantity += object.quantity
+#             object.product.save()
+#         super(BasketQuerySet, self).delete(*args, **kwargs)
 
 
 class Basket(models.Model):
@@ -9,6 +18,8 @@ class Basket(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(verbose_name="количество", default=0)
     add_datetime = models.DateTimeField(verbose_name="время добавления", auto_now_add=True)
+
+    # objects = BasketQuerySet.as_manager()
 
     @property
     def product_cost(self):
@@ -32,3 +43,20 @@ class Basket(models.Model):
     @staticmethod
     def get_items(user):
         return Basket.objects.filter(user=user).order_by("product__category")
+
+    @staticmethod
+    def get_item(pk):
+        return get_object_or_404(Basket, pk=pk)
+
+    # def save(self, *args, **kwargs):
+    #     if self.pk:
+    #         self.product.quantity -= self.quantity - self.__class__.get_item(self.pk).quantity
+    #     else:
+    #         self.product.quantity -= self.quantity
+    #     self.product.save()
+    #     super(self.__class__, self).save(*args, **kwargs)
+
+    # def delete(self):
+    #     self.product.quantity += self.quantity
+    #     self.product.save()
+    #     super(self.__class__, self).delete()
--- mainapp/models.py
+++ mainapp/models.py
@@ -18,11 +18,11 @@ class Product(models.Model):
     description = models.TextField(verbose_name="описание продукта", blank=True)
     price = models.DecimalField(verbose_name="цена продукта", max_digits=8, decimal_places=2, default=0)
     quantity = models.PositiveIntegerField(verbose_name="количество на складе", default=0)
+    reserved = models.PositiveIntegerField(verbose_name="зарезервировано", default=0)
     is_active = models.BooleanField(verbose_name="продукт активен", default=True)
 
     def __str__(self):
         return f"{self.name} ({self.category.name})"
-
 
 class Contact(models.Model):
     phone = models.CharField(max_length=50, verbose_name="номер телефона")
